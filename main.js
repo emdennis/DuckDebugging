166517ad05807d7da5bfb31997451c980217ab09
Solomon Zelenko <zelenkosolomon@gmail.com>
2019-03-21 23:36:59 -0700

    discuss app userflow

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 // local server side, baby
 // ask about copy and pasting user data
 // ask about saving user code while the app is running 
+
+// look up basic AI generation, and what 1-2 developers can reasonably do on a mostly aesthetic website 
